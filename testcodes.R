42e52243841a7f9813df679ac6c3f6e382a9c53c
SarmilanSiva <sarmilans@trabeya.com>
2017-08-31 16:13:03 +0530

    Modified Code V2

--- testcodes.R
+++ testcodes.R
@@ -8,10 +8,16 @@ summaryCSfSYM <- tmpFert %>%
   summarise(csfCountS=n(), csfAvgS=mean(PStat, na.rm=T)) %>% 
   
   ggplot(aes(x=CSf, y=csfAvgS)) +
-  geom_point(aes(size=csfCountS, colour = System), alpha=0.4) + geom_line(aes(colour = System, group=System), alpha=0.6) +
+  geom_point(aes(size=csfCountS, colour = System), alpha=0.4) +
+  geom_line(aes(colour = System, group=System), alpha=0.6) +
   ylim(c(0,1)) + theme_bw() +
-  labs(title="Summary for Body Condition on Pregnancy Status", x="Body Condition",
+  labs(title="Summary for Body Condition on Pregnancy Status",
+       x="Body Condition",
        y="Pregnancy Status")
 
 summaryCSfSYM
+
+#Made some changes to test the Version Control of GitHub ;)
+
+
 
